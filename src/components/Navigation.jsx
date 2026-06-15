6f019479afab010a9d3a62746fe5dc6e2e1c9dc0
Mohammad Wasim Ansari <wasimansari11190348@gmail.com>
2026-06-15 22:20:11 +0530

    pseudo push

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-    <nav>I am the new navabar</nav>
+    <div>Navigation
+      <p>I na te greater guy of the wolhoel word </p>
+      <nav>I am the new navabar</nav>
+      <p>I na te greater guy of the wolhoel word </p>
+      <p>I na te greater guy of the wolhoel word </p>
+      <p>I na te greater guy of the wolhoel word </p>
+      <p>I na te greater guy of the wolhoel word </p>
+    </div>
+    
+    
+
   )
 }
 

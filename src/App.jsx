546c550106ea307595b9471c99d032ba0cb31d35
Mohammad Wasim Ansari <wasimansari11190348@gmail.com>
2026-06-21 20:42:49 +0530

    Contact details form added and buttton component created.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navigation from './components/Navigation/Navigation'
 import './App.css';
 import ContactHeader from './components/ContactHeader/ContactHeader';
+import ContactForm from './components/ContactForm/ContactForm';
 
 const App = () => {
   return (
     <div>
       <Navigation/>
       <ContactHeader/>
+      <ContactForm/>
     </div>
   )
 }
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./ContactForm.module.css";
+
+
+const ContactForm = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.contact_form}></div>
+        <div className={styles.contact_mage}>
+
+        </div>
+    </section>
+  )
+}
+
+export default ContactForm
